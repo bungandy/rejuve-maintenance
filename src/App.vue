ba058e31e3027cb5eb3c6f5305887aaa17e13525
Andy <bungandy@gmail.com>
2022-11-13 18:53:05 +0700

    maintenance

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="w-screen h-screen grid place-items-center bg-gradient-to-br from-purple-500 to-purple-900 text-white">
+    <div class="grid gap-y-4 place-items-center text-center">
+      <img src="https://static.mineralcdn.net/site/rejuvecoid/rejuve_circle.png" width="100"/>
+      <h1 class="text-5xl font-bold">Be right back.</h1>
+      <p class="text-sm">We're making updates to the Re.juve Indonesia Store.</p>
+      <p class="text-sm">Check back soon.</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* .logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+} */
+</style>
