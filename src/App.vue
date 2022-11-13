aee3d80514d8ccc33735bb5d06170d4fe5ee40b2
Andy <bungandy@gmail.com>
2022-11-13 19:04:31 +0700

    change: remove 'Store' text

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <div class="grid gap-y-4 place-items-center text-center">
       <img src="https://static.mineralcdn.net/site/rejuvecoid/rejuve_circle.png" width="100"/>
       <h1 class="text-5xl font-bold">Be right back.</h1>
-      <p class="text-sm">We're making updates to the Re.juve Indonesia Store.</p>
+      <p class="text-sm">We're making updates to the Re.juve Indonesia.</p>
       <p class="text-sm">Check back soon.</p>
     </div>
   </div>
